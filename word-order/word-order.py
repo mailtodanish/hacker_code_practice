4bd13e0837550407ed2ceb83dd88f7f5c13fb9ab
Mohd Ahshan Danish <mailtodanish@gmail.com>
2022-05-14 15:55:18 +0530

    Create word-order.py

---
+++ word-order/word-order.py
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/word-order/problem?isFullScreen=true
+# 4
+# bcdef
+# abcdefg
+# bcde
+# bcdef
+from collections import Counter
+
+n = int(input())
+lst_of_words =[]
+for i in range(n):
+    lst_of_words.append(input())
+
+count =  dict(Counter(lst_of_words))
+print(len(count))
+values = list(map(str, list(count.values()))  )
+print(" ".join(values))
