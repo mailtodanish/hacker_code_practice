8664d597135a34e69b1f0cea083b0a6852d34cf9
Mohd Ahshan Danish <mailtodanish@gmail.com>
2022-05-16 22:31:34 +0530

    Update word-order.py

--- word-order/word-order.py
+++ word-order/word-order.py
@@ -1,17 +1,23 @@
-# https://www.hackerrank.com/challenges/word-order/problem?isFullScreen=true
+'''
+# TODO: https://www.hackerrank.com/challenges/word-order/problem?isFullScreen=true
+# inputs :
 # 4
 # bcdef
 # abcdefg
 # bcde
 # bcdef
+
+'''
+
+
 from collections import Counter
 
 n = int(input())
-lst_of_words =[]
+lst_of_words = []
 for i in range(n):
     lst_of_words.append(input())
 
-count =  dict(Counter(lst_of_words))
+count = dict(Counter(lst_of_words))
 print(len(count))
-values = list(map(str, list(count.values()))  )
+values = list(map(str, list(count.values())))
 print(" ".join(values))
